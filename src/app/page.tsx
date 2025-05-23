ca31459990d7da5885aeac3ad5e1b5766ec64673
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-05-23 16:42:55 -0500

    feat: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import { HydrateClient, api } from "~/trpc/server";
+
+export default async function Home() {
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        que mas
+      </main>
+    </HydrateClient>
+  );
+}
