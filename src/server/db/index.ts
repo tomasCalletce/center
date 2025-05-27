0b01fc9adad4991a1ae28ab718a1ff95392109fd
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-05-27 16:19:03 -0500

    feat: add challenges document

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,2 +1,2 @@
-export * from "~/server/db/schemas/users";
 export * from "~/server/db/schemas/documents";
+export * from "~/server/db/schemas/challenges";
---
+++ src/server/db/schemas/challenges.ts
@@ -0,0 +1,39 @@
+import {
+  pgTable,
+  timestamp,
+  varchar,
+  pgEnum,
+  uuid,
+  integer,
+  text,
+} from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export const challengeVisibilityEnum = z.enum(["VISIBLE", "HIDDEN"]);
+export const challengeVisibilityValues = challengeVisibilityEnum.Values;
+export const challengeVisibilityEnumSchema = pgEnum("challenge_visibility", [
+  "VISIBLE",
+  "HIDDEN",
+]);
+
+export const challenges = pgTable("challenges", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  _clerk: varchar("_user", { length: 32 }).notNull(),
+  _document: varchar("_document", { length: 32 }).notNull(),
+  title: varchar("title", { length: 255 }).notNull(),
+  markdown: text("markdown"),
+  price_pool: integer("price_pool").notNull(),
+  price_pool_currency: varchar("price_pool_currency", { length: 3 }).notNull(),
+  visibility: challengeVisibilityEnumSchema("visibility").notNull(),
+  deadline_at: timestamp("deadline"),
+  created_at: timestamp("created_at").defaultNow().notNull(),
+  updated_at: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const verifyChallengesSchema = createInsertSchema(challenges).omit({
+  id: true,
+  _clerk: true,
+  created_at: true,
+  updated_at: true,
+});
