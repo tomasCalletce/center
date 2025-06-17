80a64766051b2d42d9ab8853afec2d8a4e152b16
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-06-17 18:31:27 -0500

    feat: delet backend code

--- src/server/api/routers/challenge/routes.ts
+++ src/server/api/routers/challenge/routes.ts
@@ -1,14 +1,8 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { create } from "~/server/api/routers/challenge/procedures/create";
-import { all } from "~/server/api/routers/challenge/procedures/all";
-import { details } from "~/server/api/routers/challenge/procedures/details";
-import { participant } from "~/server/api/routers/challenge/procedures/participant";
 import { allSubmissions } from "~/server/api/routers/challenge/procedures/all-submission";
 
 export const challengeRouter = createTRPCRouter({
   create,
-  all,
-  details,
-  participant,
   allSubmissions,
 });
