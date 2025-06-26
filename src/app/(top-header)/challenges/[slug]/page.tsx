fbd8b2efd79812f39cbb2b20a4e75e25a8a3e708
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-06-26 17:54:52 -0500

    feat: add submition card

--- src/app/(top-header)/challenges/[slug]/page.tsx
+++ src/app/(top-header)/challenges/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { HydrateClient } from "~/trpc/server";
 import { ChallengeDetails } from "~/app/(top-header)/challenges/[slug]/_components/challenge-details";
 import { ChallengeMarkdown } from "~/app/(top-header)/challenges/[slug]/_components/challenge-markdown";
+import { ChallengeSubmissions } from "~/app/(top-header)/challenges/[slug]/_components/challenge-submissions";
 
 export default async function ChallengePage({
   params,
@@ -14,6 +15,7 @@ export default async function ChallengePage({
       <div className="w-full px-6 py-6">
         <div className="space-y-6">
           <ChallengeDetails slug={slug} />
+          <ChallengeSubmissions slug={slug} />
           <ChallengeMarkdown slug={slug} />
         </div>
       </div>
