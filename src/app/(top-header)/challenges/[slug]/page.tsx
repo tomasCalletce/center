dc6588c0fa339a9753602adbb21a23eecb97e1f0
tomas <64665732+tomasCalletce@users.noreply.github.com>
2025-06-26 17:23:43 -0500

    feat: delete not needed code

--- src/app/(top-header)/challenges/[slug]/page.tsx
+++ src/app/(top-header)/challenges/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { HydrateClient } from "~/trpc/server";
 import { ChallengeDetails } from "~/app/(top-header)/challenges/[slug]/_components/challenge-details";
-import { ChallengeParticipants } from "~/app/(top-header)/challenges/[slug]/_components/challenge-participants";
 import { ChallengeMarkdown } from "~/app/(top-header)/challenges/[slug]/_components/challenge-markdown";
-import { SignedIn } from "@clerk/nextjs";
 
 export default async function ChallengePage({
   params,
@@ -16,9 +14,6 @@ export default async function ChallengePage({
       <div className="w-full px-6 py-6">
         <div className="space-y-6">
           <ChallengeDetails slug={slug} />
-          <SignedIn>
-            <ChallengeParticipants slug={slug} />
-          </SignedIn>
           <ChallengeMarkdown slug={slug} />
         </div>
       </div>
